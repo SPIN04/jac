89b9c789ba8fef4314d581e2d3b2abb1504f33ab
SPINO <edoardo3spinelli@gmail.com>
2023-12-13 12:41:03 +0100

    progetto

---
+++ java eclipse all/pluginfreezer/src/pluginfreezer/main.java
@@ -0,0 +1,34 @@
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.Random;
+
+public class LinkCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            String randomString = generateRandomString(5);
+            player.sendMessage("Il tuo codice casuale è: " + randomString);
+        } else {
+            sender.sendMessage("Questo comando può essere eseguito solo da un giocatore.");
+        }
+        return true;
+    }
+
+    private String generateRandomString(int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new Random();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(characters.length());
+            stringBuilder.append(characters.charAt(index));
+        }
+
+        return stringBuilder.toString();
+    }
+}
