f1b5a4df28f6e24dea8c83b73c6507390b76887a
SPINO <edoardo3spinelli@gmail.com>
2024-01-19 14:48:17 +0100

    oggetto nave terminato

--- js/memory oggetti/Nave.js
+++ js/memory oggetti/Nave.js
@@ -1,34 +1,59 @@
 class Nave {
-    constructor(lunghezza=1) {
+    constructor(lunghezza = 1) {
         this.lunghezza = lunghezza;
         this.disposizione = 'x';
-        this.colpi_subiti=0
-        this.affondata=false
-        this.posizione=[0,0]
+        this.colpi_subiti = 0;
+        this.affondata = false;
+        this.posizione = [0, 0];
+        this.celle = this.ottieni_Celle();
+    }
+
+    ottieni_Celle() {
+        let celle_tmp = [];
+        if (this.disposizione === 'x') {
+            for (let i = this.posizione[1]; i < this.posizione[1] + this.lunghezza; i++) {
+                celle_tmp.push([this.posizione[0], i]);
+            }
+        }
+        if (this.disposizione === 'y') {
+            for (let i = this.posizione[0]; i < this.posizione[0] + this.lunghezza; i++) {
+                celle_tmp.push([i, this.posizione[1]]);
+            }
+        }
+        return celle_tmp;
     }
 
     get_info() {
-        let to_print= [this.lunghezza];
-        console.log(to_print)
+        console.log(this.celle);
     }
-    ruota(){
-        this.disposizione=(this.disposizione=='x')?'y':'x'
+
+    ruota() {
+        this.disposizione = (this.disposizione === 'x') ? 'y' : 'x';
+        this.celle = this.ottieni_Celle();
     }
+
     colpisci(y, x) {
         const [py, px] = this.posizione;
-    
+
         if (
-            (this.disposizione === 'x' && x > px && x <= px + this.lunghezza - 1) ||
-            (this.disposizione === 'y' && y > py && y <= py + this.lunghezza - 1)
+            (this.disposizione === 'x' && x >= px && x < px + this.lunghezza) ||
+            (this.disposizione === 'y' && y >= py && y < py + this.lunghezza)
         ) {
             this.colpi_subiti++;
             this.affondata = this.colpi_subiti >= this.lunghezza;
         }
     }
-    posiziona(y,x){
-        this.posizione.push(y,x)
+    set_celle(){
+        celle_tmpx=this.ottieni_Celle()
+        this.celle=celle_tmpx
+    }
+    deploy(y, x) {
+        this.posizione[0] = y;
+        this.posizione[1] = x;
+        this.celle = this.ottieni_Celle();
     }
 }
 
 let nave = new Nave(5);
-nave.get_info()
+
+nave.get_info();
